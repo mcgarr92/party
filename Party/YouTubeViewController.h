38b1655d47be941e3635295ae8433f872c58b8d2
Kevin <mcgarr92@students.rowan.edu>
2013-06-07 22:57:11 -0400

    second

---
+++ Party/YouTubeViewController.h
@@ -0,0 +1,18 @@
+//
+//  YouTubeViewController.h
+//  Party
+//
+//  Created by Kevin McGarry on 6/7/13.
+//  Copyright (c) 2013 kevscode dot net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YouTubeViewController : UIViewController {
+    
+    IBOutlet UITextField *searchEntry;
+    IBOutlet UIButton *submitButton;
+    IBOutlet UIWebView *webView;
+}
+
+@end
---
+++ Party/YouTubeViewController.m
@@ -0,0 +1,44 @@
+//
+//  YouTubeViewController.m
+//  Party
+//
+//  Created by Kevin McGarry on 6/7/13.
+//  Copyright (c) 2013 kevscode dot net. All rights reserved.
+//
+
+#import "YouTubeViewController.h"
+
+@interface YouTubeViewController ()
+
+@end
+
+@implementation YouTubeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)SearchButtonPressed:(id)sender {
+    
+//    results.text = [NSString stringWithFormat:@"results"];
+//    webView.
+}
+
+@end
